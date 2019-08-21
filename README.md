29ca99d13e2cfc1db8c6303e19114df176ef7f82
Naoyuki Tai <ntaigit@cleanwinner.com>
2019-08-21 12:24:01 +0000

    Created the git-shell-commands with Python. I don't understand why importing module doesn't set the __doc__ for the module. Each command must support "-h" as the "help foo" runs the command with "-h" Right now, list and create-repo (and help) are the only commands but it's plenty for my use.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # git-shell-commands
 git-shell-commands written in Python (3.6)
+
+Currently there are only 3 commands.
+
+help: Show help message
+list: List existing repositories
+create-repo: Creates an empty repository
---
+++ util.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3.6
+
+import os
+import subprocess
+
+home = os.environ['HOME']
+
+def get_git_dirs(dir=None):
+    if dir is None:
+        dir = home
+        pass
+    git_dirs = []
+    for item in os.listdir(dir):
+        a_path = os.path.join(dir, item)
+        if not os.path.isdir(a_path):
+            continue
+        if item[-4:] != ".git":
+            git_dirs = git_dirs + get_git_dirs(a_path)
+            continue
+        cmd = 'git --git-dir="{}" rev-parse --is-bare-repository'.format(a_path)
+        git = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        if git.returncode == 0:
+            if git.stdout == b'true\n':
+                git_dirs.append(a_path)
+                pass
+            pass
+        else:
+            print(git.stderr)
+            pass
+        pass
+
+    return git_dirs 
